c99553b82ed276010f64864d9169f33c050978db
RomiTita <rominaftita@gmail.com>
2017-04-14 21:40:20 -0300

    [I] Inscripcion Torneo

---
+++ 02 - Proyecto y Codigos Fuentes/01_Proyecto_Web/AplicacionWebJJSS/JJSS/JJSS/Presentacion/InscripcionTorneo2.aspx
@@ -0,0 +1,65 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Presentacion/Site.Master" AutoEventWireup="true" CodeBehind="InscripcionTorneo2.aspx.cs" Inherits="JJSS.InscripcionTorneo" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="cphEncabezado" runat="server">
+    
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="FeaturedContent" runat="server">
+    <p>
+        <asp:Label ID="lbl_Inscripcion" runat="server" Text="Inscripción" Font-Size="XX-Large" CssClass=""></asp:Label>
+    </p>
+
+    <asp:Panel ID="pnl_ingreso_datos" runat="server">
+        <p>
+        Elegir torneo:
+       <asp:DropDownList ID="ddl_torneos" runat="server"></asp:DropDownList>
+    </p>
+
+   <p>
+       Ingresar nombre:
+       <asp:TextBox ID="txt_nombre" runat="server"></asp:TextBox>
+   </p>
+    <p > 
+        Ingresar apellido:
+        <asp:TextBox ID="txt_apellido" runat="server"></asp:TextBox>
+    </p>
+        <p>
+            Sexo:
+            <asp:RadioButtonList ID="RadioButtonList1" runat="server" AutoPostBack="True" OnSelectedIndexChanged="RadioButtonList1_SelectedIndexChanged">
+                <asp:ListItem>Femenino</asp:ListItem>
+                <asp:ListItem>Masculino</asp:ListItem>
+            </asp:RadioButtonList>
+        </p>
+        <p>
+            Peso:
+            <asp:TextBox ID="txt_peso" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            Edad
+            <asp:TextBox ID="txt_edad" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            Faja:
+            <asp:DropDownList ID="ddl_fajas" runat="server">
+            </asp:DropDownList>
+        </p>
+        <p>
+            <asp:Button ID="btn_aceptar" runat="server" Text="Aceptar" Height="32px" Width="80px"/>
+            &nbsp;
+            <asp:Button ID="btn_cancelar" runat="server" Text="Cancelar" Height="32px" Width="80px" />
+        </p>
+
+    </asp:Panel>
+
+    <asp:Panel ID="pnl_inscripcion_correcta" runat="server">
+        <p>
+            La inscripción se ha realizado correctamente
+
+        </p>
+
+        <p>
+            <asp:Button ID="btn_cod_barra" runat="server" Text="Imprimir codigo de barra" Height="32px" />
+        </p>
+
+    </asp:Panel>
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="MainContent" runat="server">
+</asp:Content>
---
+++ 02 - Proyecto y Codigos Fuentes/01_Proyecto_Web/AplicacionWebJJSS/JJSS/JJSS/Presentacion/InscripcionTorneo2.aspx.designer.cs
@@ -0,0 +1,132 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace JJSS {
+    
+    
+    public partial class InscripcionTorneo {
+        
+        /// <summary>
+        /// Control lbl_Inscripcion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbl_Inscripcion;
+        
+        /// <summary>
+        /// Control pnl_ingreso_datos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel pnl_ingreso_datos;
+        
+        /// <summary>
+        /// Control ddl_torneos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddl_torneos;
+        
+        /// <summary>
+        /// Control txt_nombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_nombre;
+        
+        /// <summary>
+        /// Control txt_apellido.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_apellido;
+        
+        /// <summary>
+        /// Control RadioButtonList1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButtonList RadioButtonList1;
+        
+        /// <summary>
+        /// Control txt_peso.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_peso;
+        
+        /// <summary>
+        /// Control txt_edad.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_edad;
+        
+        /// <summary>
+        /// Control ddl_fajas.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddl_fajas;
+        
+        /// <summary>
+        /// Control btn_aceptar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_aceptar;
+        
+        /// <summary>
+        /// Control btn_cancelar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_cancelar;
+        
+        /// <summary>
+        /// Control pnl_inscripcion_correcta.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel pnl_inscripcion_correcta;
+        
+        /// <summary>
+        /// Control btn_cod_barra.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_cod_barra;
+    }
+}
